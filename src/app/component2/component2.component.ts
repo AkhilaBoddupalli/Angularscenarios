d2032fe575e177c8be1c571beb3ad83e76ff9a44
akhila <bakhila36@gmail.com>
2019-08-08 10:02:26 +0530

    scenario4 implemented

---
+++ src/app/component2/component2.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+@Component({
+ selector: 'app-component2',
+ templateUrl: './component2.component.html',
+ styleUrls: ['./component2.component.css']
+})
+export class Component2Component implements OnInit {
+  @Input('parentData') public name;
+ @Output() public childEvent = new EventEmitter();
+ public childname;
+ constructor() { }
+ ngOnInit() {
+ }
+ fireEvent()
+ {
+  
+   this.childEvent.emit(this.childname);
+ }
+}
